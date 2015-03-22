c09bb0609e671a87e733e22e98fb424d7ee4b346
David Steuber <dsteuber@gmail.com>
2015-03-22 09:38:08 -0400

    Add all project files to GitHub
    HelloUPC Xcode Project
    
    Using GitHub Apple App for initial push.

---
+++ HelloUPC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloUPC
+//
+//  Created by David Steuber on 3/22/15.
+//  Copyright (c) 2015 David Steuber.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
